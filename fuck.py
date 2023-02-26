128ed75338d0930de2b002b84579ab550d662c1d
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-02-27 00:31:05 +0600

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -1,12 +1,12 @@
-"""import os,platform
+import os,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
  print("Sorry")
- #import FILE64
-elif bit == '32bit':
- import ggg_enc
- #print("%&৳&&&*")
-"""
-from ggg_enc import ggg_enc
-ggg_enc.ggg_enc()
 
+elif bit == '32bit':
+ import ggg
+
+"""
+from ggg import ggg
+ggg.ggg()
+"""
